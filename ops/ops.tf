d702c475ab8a9a6d29749da38b107a60fca5c7ac
Rob Walk <rlwalk@gmail.com>
2019-07-05 19:54:18 -0400

    Copied main.tf from consumer example

---
+++ ops/ops.tf
@@ -0,0 +1,54 @@
+variable "aws_access_key" { }
+variable "aws_secret_key" { }
+variable "name"           { default = "dev-dyn-aws-creds" }
+
+terraform {
+  backend "remote" {
+    organization = "rlwalk"
+
+    workspaces {
+      name = "ops"
+    }
+  }
+}
+
+provider "vault" {}
+
+resource "vault_aws_secret_backend" "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  path       = "${var.name}"
+  region = "us-east-1"
+  default_lease_ttl_seconds = "120"
+  max_lease_ttl_seconds     = "240"
+}
+
+resource "vault_aws_secret_backend_role" "producer" {
+  backend = "${vault_aws_secret_backend.aws.path}"
+  name    = "${var.name}-role"
+  credential_type = "iam_user"
+
+  policy_document = <<EOF
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Effect": "Allow",
+              "Action": [
+                  "ec2:*",
+                  "iam:*"
+              ],
+              "Resource": "*"
+          }
+      ]
+  }
+  EOF
+}
+
+output "backend" {
+  value = "${vault_aws_secret_backend.aws.path}"
+}
+
+output "role" {
+  value = "${vault_aws_secret_backend_role.producer.name}"
+}
