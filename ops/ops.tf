e2381489d0be1d58e3d0683969378918fc1afe0e
Rob Walk <rlwalk@gmail.com>
2019-07-21 22:40:01 -0400

    updated aws path

--- ops/ops.tf
+++ ops/ops.tf
@@ -22,7 +22,7 @@ provider "vault" {}
 resource "vault_aws_secret_backend" "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  path       = "${var.dev-creds}-path"
+  path       = "${var.dev-creds}"
   region = "us-east-1"
   default_lease_ttl_seconds = "120"
   max_lease_ttl_seconds     = "240"
